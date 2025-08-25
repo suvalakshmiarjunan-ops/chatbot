badadc50c0b2ef060acd563146aba632ada81376
Aravind-012 <110115467+Aravind-012@users.noreply.github.com>
2025-08-25 14:35:39 +0530

    Update settings.py

--- mynewsite/settings.py
+++ mynewsite/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME':'chat_py',
         'USER':'root',
         'PASSWORD':'',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
         'OPTIONS':{
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
